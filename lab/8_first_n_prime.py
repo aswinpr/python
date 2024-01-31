6256fdcd1cf6ee3d18b44d1127ca8573e7550303
aswin_pr <97214080+aswinpr@users.noreply.github.com>
2024-01-31 05:56:21 +0530

    8_first_n_prime.py

--- lab/8_first_n_prime.py
+++ lab/8_first_n_prime.py
@@ -1,1 +1,11 @@
-.
+l=100
+n=int(input("enter the limit : "))
+count=0
+for i in range(1,l):
+    if count<n:
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i,end=" ")
+            count=count+1
